4688d5f7080ff859f4fd8d1cf38e1af79b2d48d3
raihan <5421449+ra89fi@users.noreply.github.com>
2019-03-20 13:57:02 +0600

    add FourOfour component

---
+++ src/components/FourOfour.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FourOfour = () => (
+  <p>
+    404! &nbsp; <Link to="/">Go Home</Link>
+  </p>
+);
+
+export default FourOfour;
